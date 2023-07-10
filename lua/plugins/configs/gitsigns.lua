7fd23f2f5c3077ede9c35f89d2d4ded1ccdaeb6a
huge-pancake <huge.pancake.zxr@gmail.com>
2023-07-10 18:07:35 +0800

    feat(gitsigns): clean up

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -17,35 +17,4 @@ require('gitsigns').setup {
     virt_text_pos = 'eol',
     delay = 100,
   },
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-
-    local map = require('utils').keymap.set
-
-    -- Navigation
-    map('nx', ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
-    end, { expr = true })
-
-    map('nx', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end, { expr = true })
-
-    -- Actions
-    map('n', '<leader>gs', gs.stage_hunk)
-    map('n', '<leader>gr', gs.reset_hunk)
-    map('n', '<leader>gS', gs.stage_buffer)
-    map('n', '<leader>gu', gs.undo_stage_hunk)
-    map('n', '<leader>gR', gs.reset_buffer)
-    map('n', '<leader>gp', gs.preview_hunk)
-    map('n', '<leader>gb', gs.blame_line)
-
-    -- Text object
-    map('ox', 'ic', ':<C-U>Gitsigns select_hunk<CR>')
-    map('ox', 'ac', ':<C-U>Gitsigns select_hunk<CR>')
-  end,
 }
