16c863d9a9d7ac6eae63b440155ebd20955ad727
vollowx <muffinslog@proton.me>
2023-08-01 08:43:03 +0800

    feat(bufferline): remove close buttons

--- lua/plugins/configs/bufferline.lua
+++ lua/plugins/configs/bufferline.lua
@@ -3,17 +3,18 @@ local icons = require('utils.static').icons
 
 bufferline.setup {
   options = {
+    always_show_bufferline = false,
     style_preset = bufferline.style_preset.no_italic,
     separator_style = { '', '' },
     indicator = { style = 'none' },
-    buffer_close_icon = icons.ui.Cross,
     modified_icon = icons.ui.CircleSmall,
     left_trunc_marker = icons.ui.AngleLeft,
     right_trunc_marker = icons.ui.AngleRight,
+    show_close_icon = false,
+    show_buffer_close_icons = false,
     max_name_length = 20,
     max_prefix_length = 13,
     tab_size = 20,
-    always_show_bufferline = false,
     offsets = {
       {
         filetype = 'NvimTree',
