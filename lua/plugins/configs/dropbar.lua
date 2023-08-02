5ecbc92afa9a0378271225b69401969fd7f26573
vollowx <muffinslog@proton.me>
2023-08-02 17:34:12 +0800

    feat(dropbar): clean up

--- lua/plugins/configs/dropbar.lua
+++ lua/plugins/configs/dropbar.lua
@@ -1,29 +1,8 @@
-local static = require 'utils.static'
-local dropbar = require 'dropbar'
-
-dropbar.setup {
-  general = {
-    update_interval = 64,
-    update_events = {
-      tab = {
-        'WinResized',
-      },
-    },
-  },
+require('dropbar').setup {
   bar = {
     padding = {
       left = 2,
       right = 2,
-    },
-  },
-  sources = {
-    path = {
-      modified = function(sym)
-        return sym:merge {
-          icon = static.icons.ui.DotLarge,
-          icon_hl = 'CmpItemKindSnippet',
-        }
-      end,
     },
   },
 }
