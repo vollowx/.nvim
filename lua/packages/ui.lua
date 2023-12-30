b2f276455707692fb9ffccd4f881f70a11f27e3e
vollowx <vollowx@proton.me>
2023-12-31 00:02:13 +0800

    chore(ibl): remove

--- lua/packages/ui.lua
+++ lua/packages/ui.lua
@@ -6,13 +6,6 @@ return {
   },
 
   {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    event = { 'BufReadPost', 'BufNewFile' },
-    config = function() require 'packages._.ibl' end,
-  },
-
-  {
     'Bekaboo/dropbar.nvim',
     event = { 'BufReadPost', 'BufWritePost' },
     config = function() require 'packages._.dropbar' end,
